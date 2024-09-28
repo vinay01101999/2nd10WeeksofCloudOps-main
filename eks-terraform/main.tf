264dd806568b1207ca1d2783a563f743acc1495f
vinay01101999 <129056308+vinay01101999@users.noreply.github.com>
2024-09-29 00:58:16 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -169,7 +169,7 @@ data "aws_security_group" "selected" {
     instance_types  = ["t2.small"]
 
     remote_access {
-      ec2_ssh_key               = "vsv"
+      ec2_ssh_key               = "vinay"
       source_security_group_ids = [data.aws_security_group.selected.id]
     }
 
